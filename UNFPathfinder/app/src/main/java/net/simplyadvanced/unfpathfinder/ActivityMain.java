20f107720b1dc2e87ba500f667a9fa236f6dc429
Spence Southard <Spence.Southard@gmail.com>
2014-11-27 08:24:18 -0500

    Added user location into the application

--- UNFPathfinder/app/src/main/java/net/simplyadvanced/unfpathfinder/ActivityMain.java
+++ UNFPathfinder/app/src/main/java/net/simplyadvanced/unfpathfinder/ActivityMain.java
@@ -114,7 +114,7 @@ public class ActivityMain extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.setMyLocationEnabled(true);
     }
 
     private void debug(){
